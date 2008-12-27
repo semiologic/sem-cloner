e77ba39f87d072ca3999cbbd0746ec458a987b14
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-27 11:42:32 +0000

    #147: new Sem Cloner plugin
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cloner/trunk@328 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ sem-cloner-admin.php
@@ -0,0 +1,257 @@
+<?php
+class sem_cloner_admin
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		add_action('admin_menu', array('sem_cloner_admin', 'admin_menu'));
+	} # init()
+	
+	
+	#
+	# admin_menu()
+	#
+	
+	function admin_menu()
+	{
+		add_management_page(
+			__('Clone'),
+			__('Clone'),
+			'administrator',
+			__FILE__,
+			array('sem_cloner_admin', 'admin_page')
+			);
+	} # admin_menu()
+	
+	
+	#
+	# admin_page()
+	#
+	
+	function admin_page()
+	{
+		echo '<div class="wrap">' . "\n";
+		
+		echo '<form method="post" action="">' . "\n";
+		
+		echo '<h2>' . 'Semiologic Cloner' . '</h2>' . "\n";
+		
+		if ( sem_cloner_admin::exec() )
+		{
+			$msg = <<<EOF
+
+Your site has been cloned successfully!
+
+A few options may nonetheless need some attention. In particular:
+
+- Settings / Cache
+- Settings / Google Analytics
+
+Happy blogging!
+EOF;
+			echo wpautop($msg);
+		}
+		else
+		{
+			sem_cloner_admin::start();
+		}
+		
+		echo '</form>' . "\n";
+		
+		echo '</div>' . "\n";
+	} # admin_page()
+	
+	
+	#
+	# start()
+	#
+	
+	function start()
+	{
+		wp_nonce_field('sem_cloner');
+		
+		echo '<p>' . 'The form that follows will allow you to import another Semiologic Pro site\'s options into this one. Both sites need to be running the same version of the Semiologic Pro cloner.' . '</p>' . "\n";
+		
+		echo '<p>' . 'Please enter the details of the site that you wish to copy.' . '</p>' . "\n";
+		
+		echo '<table class="form-table">' . "\n";
+		
+		foreach ( array(
+			'site_url' => 'Site Url',
+			'site_user' => 'Admin User',
+			'site_pass' => 'Admin Password',
+			)
+			as $field => $label )
+		{
+			echo '<tr valign="top">'
+				. '<th scope="row">'
+				. $label
+				. '</th>'
+				. '<td>'
+				. '<input type="' . ( $field != 'site_pass' ? 'text' : 'password' ) . '" name="' . $field . '" size="58" style="width: 90%;"'
+				. ' value="' . ( $field != 'site_pass' ? attribute_escape($_POST[$field]) : '' ) . '"'
+				. ' />'
+				. '</td>'
+				. '</tr>';
+		}
+		
+		echo '</table>' . "\n";
+		
+		echo '<p class="submit">'
+			. '<input type="submit" value="' . 'Clone Site' . '" />' . '</p>' . "\n";
+	} # start()
+	
+	
+	#
+	# exec()
+	#
+	
+	function exec()
+	{
+		if ( !$_POST ) return false;
+		
+		check_admin_referer('sem_cloner');
+		
+		foreach ( array('site_url', 'site_user', 'site_pass') as $field )
+		{
+			if ( !isset($_POST[$field]) || !$_POST[$field] )
+			{
+				echo '<div class="error">'
+					. '<p>'
+					. 'Please fill in all of the fields.'
+					. '</p>'
+					. '</div>' . "\n";
+				
+				return false;
+			}
+			
+			$$field = stripslashes($_POST[$field]);
+		}
+		
+		$url = $site_url
+			. '?method=sem_cloner'
+			. '&user=' . urlencode($site_user)
+			. '&pass=' . urlencode($site_pass)
+			. '&data=';
+		
+		foreach ( array('version', 'options') as $data )
+		{
+			$$data = wp_remote_fopen($url . $data);
+			
+			if ( is_wp_error($$data) )
+			{
+				echo '<div class="error">'
+					. '<p>' . $$data->get_error_message() . '</p>'
+					. '</div>' . "\n";
+				
+				return false;
+			}
+			
+			if ( preg_match("|<error>(.*)</error>|", $$data, $match) )
+			{
+				$msg = strip_tags($match[1]);
+				
+				echo '<div class="error">'
+					. '<p>' . $msg . '</p>'
+					. '</div>' . "\n";
+				
+				return false;
+			}
+			
+			if ( strpos($$data, "<data>") === false )
+			{
+				echo '<div class="error">'
+					. '<p>' . 'Invalid data was returned. Please make sure the Semiologic Cloner plugin is active on the site you are seeking to clone.' . '</p>'
+					. '</div>' . "\n";
+				
+				return false;
+			}
+
+			$$data = preg_replace("/^.*<data>|<\/data>.*$/", '', $$data);
+			$$data = @base64_decode($$data);
+			
+			$$data = 'return ' . $$data . ';';
+			$$data = eval($$data);
+			
+			if ( $$data === false ) return false;
+			
+			switch ( $data )
+			{
+			case 'version':
+				break;
+				if ( $$data != sem_cloner_version )
+				{
+					echo '<div class="error">'
+						. '<p>' . 'Version mismatch. Please make sure the site you are cloning is running the same version of Semiologic Cloner as this one.' . '</p>'
+						. '</div>' . "\n";
+					
+					return false;
+				}
+				break;
+			
+			case 'options':
+				foreach ( $$data as $option_name => $option_value )
+				{
+					switch ( $option_name )
+					{
+					case 'mediacaster':
+						$old_value = get_option($option_name);
+						$option_value['itunes'] = $old_value['itunes'];
+						update_option($option_name, $option_value);
+						break;
+
+					case 'sem_seo':
+						$old_value = get_option($option_name);
+						foreach ( array('title', 'keywords', 'description') as $var )
+						{
+							$option_value[$var] = $old_value[$var];
+						}
+						update_option($option_name, $option_value);
+						break;
+
+					case 'sem_api_key':
+						if ( !get_option('sem_api_key') )
+						{
+							update_option($option_name, $option_value);
+						}
+						break;
+
+					default:
+						update_option($option_name, $option_value);
+						break;
+					}
+
+					if ( $plugins = get_option('active_plugins') )
+					{
+						$old_plugin_page = $GLOBALS['plugin_page'];
+						unset($GLOBALS['plugin_page']);
+
+						foreach ( $plugins as $plugin )
+						{
+							if ( file_exists(ABSPATH . PLUGINDIR . '/' . $plugin) )
+							{
+								include_once(ABSPATH . PLUGINDIR . '/' . $plugin);
+								do_action('activate_' . $plugin);
+							}
+						}
+
+						$GLOBALS['plugin_page'] = $old_plugin_page;
+					}
+				}
+				
+				break;
+			}
+		}
+		
+		# turn cache off if it exists
+		if ( function_exists('wp_cache_disable') ) wp_cache_disable();
+		
+		return true;
+	} # exec()
+} # sem_cloner_admin
+
+sem_cloner_admin::init();
+?>
---
+++ sem-cloner.php
@@ -0,0 +1,232 @@
+<?php
+/*
+Plugin Name: Semiologic Cloner
+Plugin URI: http://www.getsemiologic.com
+Description: Lets you clone a Semiologic Pro site's preferences.
+Author: Denis de Bernardy
+Version: 1.0 beta
+Author URI: http://www.getsemiologic.com
+Update Service: http://version.semiologic.com/plugins
+Update Tag: sem_cloner
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+define('sem_cloner_version', '1.0');
+
+class sem_cloner
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		add_action('init', array('sem_cloner', 'rpc'));
+	} # init()
+	
+	
+	#
+	# rpc()
+	#
+	
+	function rpc()
+	{
+		if ( !isset($_REQUEST['method']) || $_REQUEST['method'] != 'sem_cloner' ) return;
+		
+		# Reset WP
+		$GLOBALS['wp_filter'] = array();
+		$GLOBALS['cache_enabled'] = false;
+
+		while ( @ob_end_clean() );
+
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		// always modified
+		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+		// HTTP/1.1
+		header('Cache-Control: no-store, no-cache, must-revalidate');
+		header('Cache-Control: post-check=0, pre-check=0', false);
+		// HTTP/1.0
+		header('Pragma: no-cache');
+
+		# Set the response format.
+		header( 'Content-Type:text/xml; charset=utf-8' );
+		echo '<?xml version="1.0" encoding="utf-8" ?>';
+		
+		# Validate user
+		if ( !isset($_REQUEST['user']) || !$_REQUEST['user']
+			|| !isset($_REQUEST['pass']) || !$_REQUEST['pass']
+			)
+		{
+			die('<error>Access Denied</error>');
+		}
+
+		$user = wp_authenticate($_REQUEST['user'], $_REQUEST['pass']);
+		
+		if ( is_wp_error($user)
+			|| !$user->has_cap('administrator')
+			)
+		{
+			die('<error>Access Denied</error>');
+		}
+		
+		# Execute RPC
+		sem_cloner::export();
+		die;
+	} # rpc()
+	
+	
+	#
+	# export()
+	#
+	
+	function export()
+	{
+		$data = $_REQUEST['data'];
+		
+		if ( !in_array($data, array('version', 'options')) )
+		{
+			die('<error>Invalid Request</error>');
+		}
+		
+		$$data = false;
+
+		switch ( $data )
+		{
+		case 'version':
+			$$data = sem_cloner_version;
+			break;
+
+		case 'options':
+			global $wpdb;
+			
+			$option_names = (array) $wpdb->get_col("
+				SELECT option_name
+				FROM $wpdb->options
+				WHERE option_name NOT IN (
+						'home',
+						'siteurl',
+						'blogname',
+						'blogdescription',
+						'admin_email',
+						'default_category',
+						'db_version',
+						'secret',
+						'page_uris',
+						'sem_links_db_changed',
+						'wp_autoblog_feeds',
+						'wp_hashcash_db',
+						'posts_have_fulltext_index',
+						'permalink_redirect_feedburner',
+						'sem_google_analytics_params',
+						'google_analytics',
+						'falbum_options',
+						'do_smart_ping',
+						'blog_public',
+						'countdown_datefile',
+						'remains_to_ping',
+						'rewrite_rules',
+						'upload_path',
+						'show_on_front',
+						'page_on_front',
+						'page_for_posts',
+						'sem_static_front_cache',
+						'wpcf_email',
+						'wpcf_subject_suffix',
+						'wpcf_success_msg',
+						'sem_newsletter_manager_params',
+						'semiologic',
+						'sem_docs',
+						'feedburner_settings',
+						'doing_cron',
+						'update_core',
+						'update_plugins',
+						'update_themes',
+						'version_checker',
+						'sem_versions',
+						'permalink_structure',
+						'category_base',
+						'tag_base',
+						'sem_nav_menus',
+						'sem5_nav',
+						'google_analytics',
+						'cron',
+						'fix_wysiwyg',
+						'recently_edited',
+						'script_manager',
+						'sem5_docs',
+						'sem5_docs_updated',
+						'auth_salt',
+						'dealdotcom_deal',
+						'dismissed_update_core',
+						'feedsmith_token',
+						'nowReadingVersions',
+						'recently_activated',
+						'sem_entropy',
+						'sem_docs_version',
+						'wporg_popular_tags'
+						)
+				AND option_name NOT LIKE '%cache%'
+				AND option_name NOT LIKE '%Cache%'
+				AND option_name NOT LIKE 'mailserver\_%'
+				AND option_name NOT LIKE 'sm\_%'
+				AND option_name NOT LIKE '%hashcash%'
+				AND option_name NOT LIKE 'wp\_cron\_%'
+				AND option_name NOT LIKE 'wpnavt\_%'
+				AND option_name NOT REGEXP '^rss_[0-9a-f]{32}'
+				AND option_name NOT LIKE 'IWG\_%'
+				AND option_name NOT LIKE '%\_salt'
+				AND option_name NOT LIKE '%\_seed'
+				AND option_name NOT LIKE 'search\_reloaded\_%'
+				AND option_name NOT LIKE 'SUP\_%'
+				AND option_name NOT LIKE 'xml\_sitemaps%'
+				;");
+			
+			$options = array();
+			
+			foreach ( $option_names as $option_name )
+			{
+				$option = get_option($option_name);
+				
+				if ( !is_object($option) )
+				{
+					$options[$option_name] = $option;
+				}
+			}
+			
+			$$data = $options;
+			
+			break;
+		}
+
+		$$data = var_export($$data, true);
+		$$data = $$data;
+		$$data = base64_encode($$data);
+		$$data = wordwrap($$data, 75, "\n", 1);
+		$$data = '<data>'
+			. "\n"
+			. $$data
+			. "\n"
+			. '</data>';
+
+		echo $$data;
+		
+		die;
+	} # export()
+} # sem_cloner
+
+sem_cloner::init();
+
+
+if ( is_admin() )
+{
+	include dirname(__FILE__) . '/sem-cloner-admin.php';
+}
+?>

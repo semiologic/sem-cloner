8b0e0e8c1c4ced0f1346459923b22bde0e8d98d6
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-23 11:37:35 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cloner/trunk@635 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cloner.php
+++ sem-cloner.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic Cloner
 Plugin URI: http://www.semiologic.com/software/marketing/sem-cloner/
 Description: Lets you clone a Semiologic Pro site's preferences.
-Version: 1.0.1 alpha
+Version: 1.0.1 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Package: https://members.semiologic.com/media/plugins/sem-cloner/sem-cloner.zip

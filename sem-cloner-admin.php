59a517080d467779284cb2a307990a9b7cb71dc3
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-04-08 16:57:08 +0000

    cloner tweaks
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cloner/trunk@1082 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cloner-admin.php
+++ sem-cloner-admin.php
@@ -272,6 +272,180 @@ EOF;
 		
 		return true;
 	} # exec()
+	
+	
+	#
+	# export()
+	#
+	
+	function export()
+	{
+		$data = $_REQUEST['data'];
+		
+		if ( !in_array($data, array('version', 'options')) )
+		{
+			die('<error>Invalid Request</error>');
+		}
+		
+		$$data = false;
+
+		switch ( $data )
+		{
+		case 'version':
+			$$data = sem_cloner_version;
+			break;
+
+		case 'options':
+			global $wpdb;
+			
+			$option_names = (array) $wpdb->get_col("
+				SELECT DISTINCT option_name
+				FROM $wpdb->options
+				WHERE option_name NOT IN (
+						'home',
+						'siteurl',
+						'blogname',
+						'blogdescription',
+						'admin_email',
+						'default_category',
+						'db_version',
+						'secret',
+						'page_uris',
+						'sem_links_db_changed',
+						'wp_autoblog_feeds',
+						'wp_hashcash_db',
+						'posts_have_fulltext_index',
+						'permalink_redirect_feedburner',
+						'sem_google_analytics_params',
+						'google_analytics',
+						'falbum_options',
+						'do_smart_ping',
+						'blog_public',
+						'countdown_datefile',
+						'remains_to_ping',
+						'rewrite_rules',
+						'upload_path',
+						'show_on_front',
+						'page_on_front',
+						'page_for_posts',
+						'sem_static_front_cache',
+						'wpcf_email',
+						'wpcf_subject_suffix',
+						'wpcf_success_msg',
+						'sem_newsletter_manager_params',
+						'semiologic',
+						'sem_docs',
+						'feedburner_settings',
+						'doing_cron',
+						'update_core',
+						'update_plugins',
+						'update_themes',
+						'version_checker',
+						'sem_versions',
+						'permalink_structure',
+						'category_base',
+						'tag_base',
+						'sem_nav_menus',
+						'sem5_nav',
+						'sem_nav_menus',
+						'google_analytics',
+						'cron',
+						'fix_wysiwyg',
+						'recently_edited',
+						'script_manager',
+						'sem5_docs',
+						'sem5_docs_updated',
+						'auth_salt',
+						'dealdotcom_deal',
+						'dismissed_update_core',
+						'feedsmith_token',
+						'nowReadingVersions',
+						'recently_activated',
+						'sem_entropy',
+						'sem_docs_version',
+						'wporg_popular_tags',
+						'sem_cloner_key'
+						)
+				AND option_name NOT LIKE '%cache%'
+				AND option_name NOT LIKE '%Cache%'
+				AND option_name NOT LIKE 'mailserver\_%'
+				AND option_name NOT LIKE 'sm\_%'
+				AND option_name NOT LIKE '%hashcash%'
+				AND option_name NOT LIKE 'wp\_cron\_%'
+				AND option_name NOT LIKE 'wpnavt\_%'
+				AND option_name NOT REGEXP '^rss_[0-9a-f]{32}'
+				AND option_name NOT LIKE 'IWG\_%'
+				AND option_name NOT LIKE '%\_salt'
+				AND option_name NOT LIKE '%\_seed'
+				AND option_name NOT LIKE 'search\_reloaded\_%'
+				AND option_name NOT LIKE 'SUP\_%'
+				AND option_name NOT LIKE 'xml\_sitemaps%'
+				AND option_name NOT LIKE 'uninstall\_%'
+				;");
+			
+			$options = array();
+			
+			foreach ( $option_names as $option_name )
+			{
+				$option = get_option($option_name);
+				
+				# discard objects
+				if ( sem_cloner_admin::has_object($option) )
+				{
+					continue;
+				}
+			
+				$options[$option_name] = $option;
+			}
+			
+			$$data = $options;
+			
+			break;
+		}
+
+		$$data = var_export($$data, true);
+		$$data = $$data;
+		$$data = base64_encode($$data);
+		$$data = wordwrap($$data, 75, "\n", 1);
+		$$data = '<data>'
+			. "\n"
+			. $$data
+			. "\n"
+			. '</data>';
+
+		echo $$data;
+		
+		die;
+	} # export()
+	
+	
+	#
+	# has_object()
+	#
+	
+	function has_object($option)
+	{
+		if ( is_string($option) || is_numeric($option) || is_bool($option) )
+		{
+			return false;
+		}
+		elseif ( gettype($option) == 'object' )
+		{
+			return true;
+		}
+		elseif ( is_array($option) )
+		{
+			foreach ( $option as $opt )
+			{
+				if ( sem_cloner_admin::has_object($opt) )
+				{
+					return true;
+				}
+			}
+		}
+			
+		return false;
+	} # has_object()
 } # sem_cloner_admin
 
 sem_cloner_admin::init();
--- sem-cloner.php
+++ sem-cloner.php
@@ -17,7 +17,7 @@ This software is copyright Mesoconcepts and is distributed under the terms of th
 http://www.mesoconcepts.com/license/
 **/
 
-define('sem_cloner_version', '1.2');
+define('sem_cloner_version', '1.3');
 
 class sem_cloner
 {
@@ -70,182 +70,12 @@ class sem_cloner
 		}
 
 		# Execute RPC
-		sem_cloner::export();
+		if ( !class_exists('sem_cloner') ) {
+			include_once dirname(__FILE__) . 'sem-cloner-admin.php';
+		
+		sem_cloner_admin::export();
 		die;
 	} # rpc()
-	
-	
-	#
-	# export()
-	#
-	
-	function export()
-	{
-		$data = $_REQUEST['data'];
-		
-		if ( !in_array($data, array('version', 'options')) )
-		{
-			die('<error>Invalid Request</error>');
-		}
-		
-		$$data = false;
-
-		switch ( $data )
-		{
-		case 'version':
-			$$data = sem_cloner_version;
-			break;
-
-		case 'options':
-			global $wpdb;
-			
-			$option_names = (array) $wpdb->get_col("
-				SELECT DISTINCT option_name
-				FROM $wpdb->options
-				WHERE option_name NOT IN (
-						'home',
-						'siteurl',
-						'blogname',
-						'blogdescription',
-						'admin_email',
-						'default_category',
-						'db_version',
-						'secret',
-						'page_uris',
-						'sem_links_db_changed',
-						'wp_autoblog_feeds',
-						'wp_hashcash_db',
-						'posts_have_fulltext_index',
-						'permalink_redirect_feedburner',
-						'sem_google_analytics_params',
-						'google_analytics',
-						'falbum_options',
-						'do_smart_ping',
-						'blog_public',
-						'countdown_datefile',
-						'remains_to_ping',
-						'rewrite_rules',
-						'upload_path',
-						'show_on_front',
-						'page_on_front',
-						'page_for_posts',
-						'sem_static_front_cache',
-						'wpcf_email',
-						'wpcf_subject_suffix',
-						'wpcf_success_msg',
-						'sem_newsletter_manager_params',
-						'semiologic',
-						'sem_docs',
-						'feedburner_settings',
-						'doing_cron',
-						'update_core',
-						'update_plugins',
-						'update_themes',
-						'version_checker',
-						'sem_versions',
-						'permalink_structure',
-						'category_base',
-						'tag_base',
-						'sem_nav_menus',
-						'sem5_nav',
-						'google_analytics',
-						'cron',
-						'fix_wysiwyg',
-						'recently_edited',
-						'script_manager',
-						'sem5_docs',
-						'sem5_docs_updated',
-						'auth_salt',
-						'dealdotcom_deal',
-						'dismissed_update_core',
-						'feedsmith_token',
-						'nowReadingVersions',
-						'recently_activated',
-						'sem_entropy',
-						'sem_docs_version',
-						'wporg_popular_tags',
-						'sem_cloner_key'
-						)
-				AND option_name NOT LIKE '%cache%'
-				AND option_name NOT LIKE '%Cache%'
-				AND option_name NOT LIKE 'mailserver\_%'
-				AND option_name NOT LIKE 'sm\_%'
-				AND option_name NOT LIKE '%hashcash%'
-				AND option_name NOT LIKE 'wp\_cron\_%'
-				AND option_name NOT LIKE 'wpnavt\_%'
-				AND option_name NOT REGEXP '^rss_[0-9a-f]{32}'
-				AND option_name NOT LIKE 'IWG\_%'
-				AND option_name NOT LIKE '%\_salt'
-				AND option_name NOT LIKE '%\_seed'
-				AND option_name NOT LIKE 'search\_reloaded\_%'
-				AND option_name NOT LIKE 'SUP\_%'
-				AND option_name NOT LIKE 'xml\_sitemaps%'
-				AND option_name NOT LIKE 'uninstall\_%'
-				;");
-			
-			$options = array();
-			
-			foreach ( $option_names as $option_name )
-			{
-				$option = get_option($option_name);
-				
-				# discard objects
-				if ( sem_cloner::has_object($option) )
-				{
-					continue;
-				}
-			
-				$options[$option_name] = $option;
-			}
-			
-			$$data = $options;
-			
-			break;
-		}
-
-		$$data = var_export($$data, true);
-		$$data = $$data;
-		$$data = base64_encode($$data);
-		$$data = wordwrap($$data, 75, "\n", 1);
-		$$data = '<data>'
-			. "\n"
-			. $$data
-			. "\n"
-			. '</data>';
-
-		echo $$data;
-		
-		die;
-	} # export()
-	
-	
-	#
-	# has_object()
-	#
-	
-	function has_object($option)
-	{
-		if ( is_string($option) || is_numeric($option) || is_bool($option) )
-		{
-			return false;
-		}
-		elseif ( gettype($option) == 'object' )
-		{
-			return true;
-		}
-		elseif ( is_array($option) )
-		{
-			foreach ( $option as $opt )
-			{
-				if ( sem_cloner::has_object($opt) )
-				{
-					return true;
-				}
-			}
-		}
-			
-		return false;
-	} # has_object()
 } # sem_cloner
 
 sem_cloner::init();

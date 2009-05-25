71615b5710f15253b88c833b38ca49bb98dcc3ae
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-25 16:58:45 +0000

    miscellaneous tweaks
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cloner/trunk@1536 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cloner.php
+++ sem-cloner.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Semiologic Cloner
 Plugin URI: http://www.semiologic.com/software/sem-cloner/
-Description: Lets you clone a Semiologic Pro site's preferences.
+Description: Lets you clone a Semiologic Pro site. It also works with normal WordPress installations, but cloning non-Semiologic Pro plugins and themes may result in unexpected behaviors.
 Version: 1.4 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com

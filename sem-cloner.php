88e88c25c5237bc59801d2e885f9ec80531b05e7
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-10 16:12:24 +0000

    misc: fix incorrect load_plugin_textdomain calls
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-cloner/trunk@2047 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-cloner.php
+++ sem-cloner.php
@@ -20,7 +20,7 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-load_plugin_textdomain('sem-cloner', null, dirname(__FILE__) . '/lang');
+load_plugin_textdomain('sem-cloner', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 
 /**

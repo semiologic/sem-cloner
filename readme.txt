0e2e434838a123fa8c999fafbb107e62604b3758
Mike Koepke <mike.koepke@gmail.com>
2014-10-26 16:23:39 -0500

    wp 4.0 compat

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Denis-de-Bernardy, Mike_Koepke
 Donate link: http://www.semiologic.com/partners/
 Tags: semiologic
 Requires at least: 2.8
-Tested up to: 3.9
+Tested up to: 4.0
 Stable tag: trunk
 
 Clone your site, plain and simple.
@@ -34,6 +34,10 @@ The [Semiologic forum](http://forum.semiologic.com) is the best place to report 
 
 
 == Change Log ==
+
+= 1.4.8 =
+
+- WP 4.0 compat
 
 = 1.4.7 =
 
--- sem-cloner.php
+++ sem-cloner.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic Cloner
 Plugin URI: http://www.semiologic.com/software/sem-cloner/
 Description: Lets you clone a Semiologic Pro site. It also works with normal WordPress installations. Cloning non-Semiologic Pro plugins and themes may result in unexpected behaviors.
-Version: 1.4.7
+Version: 1.4.8
 Author: Denis de Bernardy & Mike Koepke
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-cloner
